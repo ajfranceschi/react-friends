fb767b287d7b098604e2c521c701e837d7f751ff
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-21 00:10:14 -0400

    removed comments from Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -10,15 +10,4 @@ const Hello = (props) => {
     );
 }
 
-// class Hello extends Component {
-//     render() {
-//         return(
-//             <div className='tc'>
-//                 <h1 className='f1'>{this.props.greeting}</h1>
-//                 <p className='f3'>Welcome to React!</p>
-//             </div>
-//         );
-//     };
-// };
-
 export default Hello;
