2f12a7d05767ae32015083b959acd6be31011191
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-21 00:04:22 -0400

    installed tachyons and created first component. <Hello />

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Hello extends Component {
+    render() {
+        return(
+            <div className='tc'>
+                <h1 className='f1'>Hello from Hello Component!</h1>
+                <p className='f3'>Welcome to React!</p>
+            </div>
+        );
+    };
+};
+
+export default Hello;
