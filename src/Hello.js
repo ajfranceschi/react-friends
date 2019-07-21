7663d685d3f3f0bf01e34af130a7261bd10a9f90
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-21 00:06:55 -0400

    added prop with greeting

--- src/Hello.js
+++ src/Hello.js
@@ -4,7 +4,7 @@ class Hello extends Component {
     render() {
         return(
             <div className='tc'>
-                <h1 className='f1'>Hello from Hello Component!</h1>
+                <h1 className='f1'>{this.props.greeting}</h1>
                 <p className='f3'>Welcome to React!</p>
             </div>
         );
