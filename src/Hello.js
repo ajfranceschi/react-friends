b31f97840699b3e93dfeca1116712b370882f689
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-21 00:08:48 -0400

    changed Hello component's class to function

--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 
-class Hello extends Component {
-    render() {
-        return(
-            <div className='tc'>
-                <h1 className='f1'>{this.props.greeting}</h1>
-                <p className='f3'>Welcome to React!</p>
-            </div>
-        );
-    };
-};
+
+const Hello = (props) => {
+    return(
+        <div className='tc'>
+            <h1 className='f1'>{props.greeting}</h1>
+            <p className='f3'>Welcome to React!</p>
+        </div>
+    );
+}
+
+// class Hello extends Component {
+//     render() {
+//         return(
+//             <div className='tc'>
+//                 <h1 className='f1'>{this.props.greeting}</h1>
+//                 <p className='f3'>Welcome to React!</p>
+//             </div>
+//         );
+//     };
+// };
 
 export default Hello;
