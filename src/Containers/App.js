dd9ce251b6af115958456071e2fbb89b697db124
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-28 22:32:08 -0400

    finished configuring redux

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -6,37 +6,43 @@ import SearchBox from '../Components/SearchBox';
 import './App.css';
 import Scroll from '../Components/Scroll';
 import ErrorBoundary from "./ErrorBoundary";
-import {setSearchField} from "../actions";
+import {setSearchField, requestRobots} from "../actions";
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = dispatch => {
     //send action
     return {
-        searchBoxDidChange: (event) => dispatch(setSearchField(event.target.value))
+        searchBoxDidChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            robots: []/*,
-            searchField: ''*/
-        }
-    }
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: []/*,
+    //         searchField: ''*/
+    //     }
+    // }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {
-                this.setState({ robots: users })
-            })
+        this.props.onRequestRobots();
+
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(response => response.json())
+        //     .then(users => {
+        //         this.setState({ robots: users })
+        //     })
 
     }
 
@@ -47,15 +53,15 @@ class App extends Component {
     // }
 
     render() {
-        const {robots} = this.state;
-        const {searchField} = this.props;
+        // const {robots} = this.state;
+        const {robots, searchField, isPending, /*error*/} = this.props;
 
 
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
 
-        return robots.length === 0
+        return /*robots.length*/ isPending === true
             ? <h1>Loading</h1>
             : (<div className='tc'>
                 <h1 className='light-green'>REACTING FRIENDS</h1>
--- src/reducers.js
+++ src/reducers.js
@@ -1,18 +1,45 @@
 // CONSTANTS
-import {CHANGE_SEARCH_FIELD} from "./constants";
+import {CHANGE_SEARCH_FIELD, REQUEST_ROBOTS_FAILED, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_PENDING} from "./constants";
 
 // create an initial state
-const initialState = {
+const initialStateSearch = {
     searchField: ''
 }
 
 //searchRobots reducer is a function that is going to be used in multiple places
-export const searchRobots = (currentState = initialState, action = {}) => {
+export const searchRobots = (currentState = initialStateSearch, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             // return Object.assign({}, currentState, {searchField: action.payload})
             return {...currentState, searchField: action.payload};
         default:
             return currentState;
+    }
+}
+
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+    switch (action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, initialStateRobots, {
+                isPending: true
+            });
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, initialStateRobots, {
+                isPending: false,
+                robots: action.payload
+            })
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, initialStateRobots, {
+                error: action.payload,
+                isPending: false
+            })
+        default:
+            return initialStateRobots;
     }
 }
